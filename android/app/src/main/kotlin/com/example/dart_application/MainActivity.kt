c6928d339a9e8728746a970edadfa16e44227a86
zinho <osmelzinho@gmail.com>
2022-01-17 13:06:44 -0400

    first commi

---
+++ android/app/src/main/kotlin/com/example/dart_application/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.dart_application
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ test/test.dart
@@ -0,0 +1,17 @@
+import 'dart:math';
+
+main() {
+  int tot = 0;
+
+  Random random = Random();
+  int number = random.nextInt(50);
+  print("Número Gerado: $number");
+  print("Números divisíveis por 3 ou 5 menores que $number:");
+  for (int i = 1; i < number; i++) {
+    if (i % 3 == 0 || i % 5 == 0) {
+      tot += i;
+      print(i);
+    }
+  }
+  print("Somatóri : $tot");
+}
